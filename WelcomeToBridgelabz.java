e8243ea27cdac34e9e7e95c6c31e5e9db56ed22b
jitesh50106 <jiteshjain9259@gmail.com>
2026-01-11 12:46:32 +0530

    [Jitesh]Add.Print Welcome to Bridgelabz program

--- WelcomeToBridgelabz.java
+++ WelcomeToBridgelabz.java
@@ -1,9 +1,9 @@
-public class WelcomeToBridgelabz {
+public class WelcomeToBridgelabz{
     public static void m1() {
       System.out.println("Welcome to bridgelabz!");
    }
 
-   public static void main(String[] var0) {
+   public static void main(String[] args) {
       m1();
    }
     
