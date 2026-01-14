9f1271bd1133da959c9776089e2d5b97488c78f4
jitesh50106 <jiteshjain9259@gmail.com>
2026-01-14 22:44:29 +0530

    [Jitesh]Add.Calculate Average Percentage

---
+++ basic-programs/Average.java
@@ -0,0 +1,11 @@
+public class Average{
+    public static void main(String[] args) {
+        int m = 94;
+        int p = 95;
+        int c = 96;
+        int t = m + p + c;
+        int total = 100*3;
+        double a = (t*100)/total;
+        System.out.println("Sam’s average mark in PCM is " + a + "%");
+    }
+}
