dfa9f40e6b22189823dea4be3fa6cb477373c37c
jitesh50106 <jiteshjain9259@gmail.com>
2026-01-14 21:51:31 +0530

    [Jitesh]Add.Calculate age

---
+++ basic-programs/Age.java
@@ -0,0 +1,8 @@
+public class Age{
+    public static void main(String[] args) {
+        int x = 2000;
+        int y = 2024;
+        int z= y-x;
+        System.out.println("Harry's age in 2024 is " + z);
+    }
+}
