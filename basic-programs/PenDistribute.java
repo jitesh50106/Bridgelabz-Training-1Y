b2b8a298d14df7e69fede802b4944f49454e0565
jitesh50106 <jiteshjain9259@gmail.com>
2026-01-14 22:50:01 +0530

    [Jitesh]Add.Calculate The pen distribution

---
+++ basic-programs/PenDistribute.java
@@ -0,0 +1,9 @@
+public class PenDistribute{
+    public static void main(String[] args) {
+        int pen =14;
+        int s = 3;
+        int p = pen/s;
+        int r = pen%s;
+        System.out.println("The Pen Per Student is "+ p +" and the remaining pen not distributed is "+r );
+    }
+}
