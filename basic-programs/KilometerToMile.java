588aca90a8c3fe64de7e5839595cff86ab68cf43
jitesh50106 <jiteshjain9259@gmail.com>
2026-01-14 22:49:17 +0530

    [Jitesh]Add.Convert Km to Mile

---
+++ basic-programs/KilometerToMile.java
@@ -0,0 +1,7 @@
+public class KilometerToMile{
+    public static void main(String[] args) {
+        double x = 10.8;
+        double m = x*1.6;
+        System.out.println("The distance " + x + " km in miles is " + m);
+    }
+}
