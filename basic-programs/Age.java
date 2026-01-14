6bbfe5d257f5b5fefe50179e5ed93f29fd18bb72
jitesh50106 <jiteshjain9259@gmail.com>
2026-01-14 22:27:01 +0530

    Age

--- basic-programs/Age.java
+++
@@ -1,8 +0,0 @@
-public class Age{
-    public static void main(String[] args) {
-        int x = 2000;
-        int y = 2024;
-        int z= y-x;
-        System.out.println("Harry's age in 2024 is " + z);
-    }
-}
