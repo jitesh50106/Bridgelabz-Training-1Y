2ed63ef6fdb76f924262b3cab259bc37280b7d40
jitesh50106 <jiteshjain9259@gmail.com>
2026-01-21 22:32:07 +0530

    Change The Character in ascii Value and print it

---
+++ ChangeInAscii.java
@@ -0,0 +1,10 @@
+public class ChangeInAscii{
+    public static void main(String[] args) {
+        char a = args[0].charAt(0);
+        char b = args[1].charAt(0);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(args[0]+args[1]);
+        System.out.println(a + b);
+    }
+}
