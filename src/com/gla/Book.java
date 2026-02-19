ca5044ceaeb805b90b92c007f4ea7efe23b89fb6
jitesh50106 <jiteshjain9259@gmail.com>
2026-02-19 15:46:32 +0530

    [Jitesh]add.Library management

---
+++ src/com/gla/Book.java
@@ -0,0 +1,58 @@
+package com.gla;
+
+public class Book {
+        private Category category;
+        private int id;
+        private String tittle;
+        private String author;
+        private String publisher;
+        private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
---
+++ src/com/gla/LibraryService.java
@@ -0,0 +1,16 @@
+package com.gla;
+
+public class LibraryService {
+    public Boolean issueBook(Book book) {
+        return true;
+
+    }
+    public Boolean returnBook(Book book) {
+        return true;
+
+    }
+    public Boolean setRepository(Library library){
+        return true;
+    }
+
+}
