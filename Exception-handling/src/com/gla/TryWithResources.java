1c6716c367007b606beb687417593ae01baebc29
jitesh50106 <jiteshjain9259@gmail.com>
2026-03-25 22:37:35 +0530

    [Jitesh]Add.Use of Exceptional handling

---
+++ Exception-handling/src/com/gla/TryWithResources.java
@@ -0,0 +1,18 @@
+package com.gla;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class TryWithResources{
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+            String line = br.readLine();
+            if (line != null) {
+                System.out.println(line);
+            } else {
+                System.out.println("File is empty");
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
