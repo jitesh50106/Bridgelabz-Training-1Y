e66f15be76c9e8755ea623efc3a1e3fec1fa144e
jitesh50106 <jiteshjain9259@gmail.com>
2026-04-13 22:59:42 +0530

    [Jitesh]add. Create a generic box and store value

---
+++ src/Generics/Main.java
@@ -0,0 +1,27 @@
+package Generics;
+
+public class Main {
+    static class Box<T> {
+        private T value;
+
+        public void set(T value) {
+            this.value = value;
+        }
+
+        public T get() {
+            return value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> intBox = new Box<>();
+        intBox.set(10);
+        System.out.println("Integer: " + intBox.get());
+        Box<String> strBox = new Box<>();
+        strBox.set("Hello");
+        System.out.println("String: " + strBox.get());
+        Box<Double> doubleBox = new Box<>();
+        doubleBox.set(5.5);
+        System.out.println("Double: " + doubleBox.get());
+    }
+}
